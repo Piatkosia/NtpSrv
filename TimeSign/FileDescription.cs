0aa4340c4a0e4644ae260e80348e6b688ae2fba7
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2017-12-02 16:08:50 +0100

    Przygotowanie struktury pod zapis

---
+++ TimeSign/FileDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSign
+{
+    public class FileDescription
+    {
+        public string FileName { get; set; }
+        public string Checksum { get; set; }
+
+        public DateTime SignTime { get; set; }
+
+        public string SyncFrom { get; set; } = "localhost";
+
+
+    }
+}
---
+++ TimeSign/Sign.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TimeSrvLib;
+using System.Security.Cryptography;
+
+namespace TimeSign
+{
+    class Sign
+    {
+        /// <summary>
+        /// Zwraca opis pliku pozwalający na zweryfikowanie czasu
+        /// </summary>
+        /// <param name="path">ścieżka lokalna do pliku do podpisania</param>
+        /// <returns>ścieżka dostępu do podpisu danego pliku</returns>
+        public FileDescription TagLocalFile(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("We have no path:(");
+            if (!File.Exists(path)) throw new FileNotFoundException("We have no " + path + "on local machine", path);
+            FileDescription description = new FileDescription();
+            TimeRefresher refresher = new TimeRefresher();
+            DateTime tm = DateTime.Now;
+            string ServerPath = "time.windows.com"; //potem się z configa pociągnie
+            var IsSet = refresher.RefreshOSTime(ServerPath);
+            if (IsSet)
+            {
+                description.SyncFrom = ServerPath;
+                tm = refresher.LastSet;
+            }
+            description.SignTime = tm;
+            description.FileName = path;
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    description.Checksum = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+
+            //podpisz go i zapisz do pliku
+            //zwróć ścieżkę dostępu do pliku
+            return description;
+        }
+    }
+}
